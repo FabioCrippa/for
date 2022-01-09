7b87458e743ad81b4a201334909b536b869693c3
FabioCrippa <crippa.fabiocrippa@gmail.com>
2022-01-09 16:23:05 -0300

    primeiro commit

---
+++ exercicios.js
@@ -0,0 +1,31 @@
+//estrutura "for"
+//for(let = valor inicial do laco; condicaoParada(fim do laco); incremento ou decremento){
+//}   let = i = 1                ; i < 11                     ; i++
+
+//tabuada
+let valor = 7;
+    for (let i = 1; i < 11; i++) {
+       console.log(valor + "x" + i + "=" + valor * i)
+}
+//i++ => i = i + 1 
+//i++ => i = 1 + 1
+//i++ => i = 2 + 1
+//i++ => i = 3 + 1
+//i++ => i = 4 + 1
+//i++ => i = 5 + 1
+//i++ => i = 6 + 1
+//i++ => i = 7 + 1
+//i++ => i = 8 + 1
+//i++ => i = 9 + 1
+    //i < 11 = 10 repeticoes
+
+//imprime 10 vezes "Crippa"
+for (let i = 1; i <= 10; i++) {
+    console.log("Crippa");  
+}
+
+//imprime a soma de 5 + 1 
+let valor = 5
+for (let i = 1; i < 6; i++) {
+    console.log(valor);    
+}
